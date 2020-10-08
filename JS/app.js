541fe0fe4fe7af203db6667cee0e950eb45701af
Joseph S <joe.pm.pro@gmail.com>
2020-10-08 10:40:00 +0200

    jumbotron transition

--- JS/app.js
+++ JS/app.js
@@ -80,15 +80,32 @@ var acceuil = {
   </div>
   <br>
   <!--Presentation jumbotron-->
-  <div class="jumbotron mjjJumbo">
+<div class="jumbotron mjjJumbo">    
   <h1 class="display-4">Digital Books!</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </div>
+  <transition name="jumboShow">
+    <div v-show="jumboShow">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a @click="jumboShow =! jumboShow" class="btn btn-primary btn-lg" role="button">Qui sommes-nous?</a>
+    </div>
+  </transition>
+
+  <transition name="jumboShow">
+    <div v-show="!jumboShow" class="jumboShowInfo">
+    <p>On est</p>
+    <a @click="jumboShow =! jumboShow" class="btn btn-light btn-lg" role="button">Souhaitez-vous nous contacter</a>
+    </div>
+  </transition>
 </div>
-  `
+`,
+
+  data:
+    function () {
+      return ({
+        jumboShow: true,
+      })
+    },
 }
 
 /***************************BOUTIQUE COMPONENTS********************************************/
